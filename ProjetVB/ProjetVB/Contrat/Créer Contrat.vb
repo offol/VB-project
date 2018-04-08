ed40b5c8563df1c345f7a9b81d6fd4c04492bb8f
François-Xavier Mousseau <fxmousseau@hotmail.com>
2018-04-08 18:17:26 -0400

    Affichage equipement dans contrat

--- ProjetVB/ProjetVB/Contrat/Créer Contrat.vb
+++ ProjetVB/ProjetVB/Contrat/Créer Contrat.vb
@@ -1,39 +1,18 @@
 ﻿Public Class Créer_Contrat
 
-
-    Private Sub Button_NewLigneFac_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox_pariculier.CheckedChanged
-
-    End Sub
-
-    Private Sub DaujourdhuiToolStripMenuItem_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub AutresToolStripMenuItem_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub PaiementToolStripMenuItem_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub TaxeToolStripMenuItem_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
-
-    End Sub
-
-    Private Sub btnEquipement_Click(sender As Object, e As EventArgs) Handles btnEquipement.Click
-        Dim Equip As New Équipement() With {
+    Private WithEvents Equip As New Équipement() With {
           .MdiParent = Form1
       }
+
+    Private Sub btnEquipement_Click(sender As Object, e As EventArgs) Handles btnEquipement.Click
+
         Equip.Show()
+
+    End Sub
+
+    Private Sub Equip_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Equip.FormClosed
+        DataGridView_LignesEquipement.DataSource = Equip.Listecurrentequip
+
     End Sub
 
     Private Sub Créer_Contrat_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -57,19 +36,11 @@
         Commercial.Show()
     End Sub
 
-    Private Sub Btn_supprimer_Click(sender As Object, e As EventArgs) Handles Btn_supprimer.Click
-
-    End Sub
-
     Private Sub btnService_Click(sender As Object, e As EventArgs) Handles btnService.Click
         Dim Serv As New Service() With {
           .MdiParent = Form1
       }
         Serv.Show()
-    End Sub
-
-    Private Sub btn_cliParticulier_Click_1(sender As Object, e As EventArgs) Handles btn_cliParticulier.Click
-
     End Sub
 
     Private Sub btnComplete_Click(sender As Object, e As EventArgs) Handles btnComplete.Click
@@ -80,22 +51,6 @@
         Me.Close()
     End Sub
 
-    Private Sub btn_cliCommercial_Click_1(sender As Object, e As EventArgs) Handles btn_cliCommercial.Click
-
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub NumContrat_TextChanged(sender As Object, e As EventArgs) Handles NumContrat.TextChanged
-
-    End Sub
-
-    Private Sub DataGridView1_CellContentClick_1(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
-
-    End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Dim Livraison As New Mise_en_Service() With {
            .MdiParent = Form1
@@ -104,7 +59,4 @@
 
     End Sub
 
-    Public Sub DataGridView_LigneFacture_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView_LignesEquipement.CellContentClick
-
-    End Sub
 End Class
--- ProjetVB/ProjetVB/Équipement/Équipement.vb
+++ ProjetVB/ProjetVB/Équipement/Équipement.vb
@@ -1,12 +1,21 @@
 ﻿Public Class Équipement
 
-
+    Public Listecurrentequip As List(Of Equip_Object)
+    Private BDEquiplist As List(Of Equip_Object)
 
     Private Sub ListBox8_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox8.SelectedIndexChanged
 
     End Sub
 
     Public Sub Btn_servicesave_Click(sender As Object, e As EventArgs) Handles Btn_Equipsave.Click
+
+        Listecurrentequip = New List(Of Equip_Object)
+
+        If CB_équip1M.Checked = True Then
+            Listecurrentequip.Add(BDEquiplist.Where(Function(i) i.NumEquip = 1 AndAlso i.Location = "Annuelle").First)
+        ElseIf CB_équip1A.Checked = True Then
+            Listecurrentequip.Add(BDEquiplist.Where(Function(r) r.NumEquip = 1 AndAlso r.Location = "Mensuelle").First)
+        End If
 
         Dim EquipAdd As New Confication_Équip_Ajouté() With {
            .MdiParent = Form1
@@ -16,97 +25,6 @@
     End Sub
 
     Private Sub Équipement_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim BDEquiplist As List(Of Equip_Object) = DataBase.LignesEquip
-
-        Dim Listecurrentequip As List(Of Equip_Object) = New List(Of Equip_Object)
-
-        If CB_équip1M.Checked = True Then
-            Listecurrentequip.Add(BDEquiplist.Where(Function(i) i.NumEquip = 1).First)
-        ElseIf CB_équip1A.Checked = True Then
-            Listecurrentequip.Add(BDEquiplist.Where(Function(r) r.Location = "Mensuelle"))
-        End If
-
-        Créer_Contrat.DataGridView_LignesEquipement.DataSource = Listecurrentequip
-
-    End Sub
-
-    Public Sub CB_équip1M_CheckedChanged(sender As Object, e As EventArgs) Handles CB_équip1M.CheckedChanged
-
-    End Sub
-
-    Public Sub CB_équip1A_CheckedChanged(sender As Object, e As EventArgs) Handles CB_équip1A.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox4_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox4.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox5_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox5.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox6_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox6.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox7_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox7.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox8_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox8.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox9_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox9.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox10_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox10.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox11_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox11.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox12_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox12.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox13_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox13.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox14_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox14.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox15_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox15.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox16_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox16.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox17_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox17.CheckedChanged
-
-    End Sub
-
-    Public Sub CheckBox18_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox18.CheckedChanged
-
+        BDEquiplist = DataBase.LignesEquip
     End Sub
 End Class
