d06b32bb810ddc7252d6e1863b6f820c6748df40
Unknown <JeanMarc.Hebert1996@gmail.com>
2018-04-09 09:50:32 -0400

    Objet Contrat
    Création de l'objet Contrat

---
+++ ProjetVB/ProjetVB/Contrat/Contrat.vb
@@ -0,0 +1,9 @@
+﻿Public Class Contrat
+	Private NoContrat As Integer
+	Private TypeContrat As String
+	Private DateOuverture As Date
+	Private DateResiliation As Date
+	Private Equipements As List(Of Equip_Object)
+	Private Services As List(Of ServiceObject)
+	Private DateMiseEnVigueur As Date
+End Class
